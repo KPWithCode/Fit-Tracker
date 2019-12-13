1ad4e2793a11433bdeaa7d7acedb4c2e92232416
Kyle Phillips <khillips375@yahoo.com>
2019-12-12 21:59:11 -0500

    Correct import and routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from "./components/navbar"
-import ExercisesList from "./components/exerciseslist";
-import EditExercise from "./components/editexercise";
-import CreateExercise from "./components/createexercise";
-import CreateUser from "./components/createuser"
 
-import "bootstrap/dist/css/boostrap.min.css"
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar"
+import ExercisesList from "./components/Exercises"
+import EditExercise from "./components/Editexercise"
+import CreateExercise from "./components/Createexcercise"
+import CreateUser from "./components/CreateUser"
+
+
+
 function App() {
   return (
     <Router>
+      <div className="container">
       <Navbar />
       <br />
       <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
       <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
