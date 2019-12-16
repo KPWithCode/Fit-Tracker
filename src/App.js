14936628b55da606a800f547979987c8f24fe7d2
Kyle Phillips <khillips375@yahoo.com>
2019-12-16 15:08:49 -0500

    Added class with UI gradient

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar"
 import ExercisesList from "./components/Exercises"
@@ -12,6 +12,7 @@ import CreateUser from "./components/CreateUser"
 function App() {
   return (
     <Router>
+      <div className="background" style={{height:'100vh',width:'100vw'}}>
       <div className="container">
         <Navbar />
         <br />
@@ -19,6 +20,7 @@ function App() {
         <Route path="/edit/:id" component={EditExercise} />
         <Route path="/create" component={CreateExercise} />
         <Route path="/user" component={CreateUser} />
+      </div>
       </div>
     </Router>
   );
