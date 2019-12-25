b155f7390fe6b2ecd2c65df436a2610a9e38c178
Kyle Phillips <khillips375@yahoo.com>
2019-12-24 21:34:36 -0500

    New routes added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar"
-import ExercisesList from "./components/Exercises"
+import ExercisesList from "./components/ExerciseList";
 import EditExercise from "./components/Editexercise"
 import CreateExercise from "./components/Createexercise"
 import CreateUser from "./components/CreateUser"
-
+import Home from './components/Home';
 
 
 function App() {
   return (
     <Router>
-      <div className="background" style={{height:'100vh',width:'100vw'}}>
-      <div className="container">
-        <Navbar />
-        <br />
-        <Route path="/" exact component={ExercisesList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
-        <Route path="/user" component={CreateUser} />
-      </div>
+      <div className="background" style={{ height: '100%', width: '100%' }}>
+        <div className="container">
+          <Navbar />
+          <br />
+          <Route path="/" exact component={Home} />
+          <Route path="/clientlog" component={ExercisesList} />
+          <Route path="/edit/:id" component={EditExercise} />
+          <Route path="/create" component={CreateExercise} />
+          <Route path="/user" component={CreateUser} />
+        </div>
       </div>
     </Router>
   );
