4692794b9d8bc05db1b8d5a42d362cb041fde0ec
Kyle Phillips <khillips375@yahoo.com>
2019-12-29 15:19:53 -0500

    Removed framer motion import and logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import Navbar from "./components/Navbar"
 import ExercisesList from "./components/ExerciseList";
 import EditExercise from "./components/Editexercise"
@@ -8,19 +8,23 @@ import CreateExercise from "./components/Createexercise"
 import CreateUser from "./components/CreateUser"
 import Home from './components/Home';
 
-
 function App() {
+
   return (
     <Router>
       <div className="background">
         <div className="container">
           <Navbar />
           <br />
+       
+          <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/clientlog" component={ExercisesList} />
           <Route path="/edit/:id" component={EditExercise} />
           <Route path="/create" component={CreateExercise} />
           <Route path="/user" component={CreateUser} />
+          </Switch>
+      
         </div>
       </div>
     </Router>
