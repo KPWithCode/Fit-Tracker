926842763f58b4bd10dd4a8f6eab5f7a4cf9164e
Kyle Phillips <khillips375@yahoo.com>
2019-12-29 17:02:14 -0500

    Animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css'
-
+import { motion } from "framer-motion";
 const useAnimation = (duration) => {
 
     const [progress, setProgress] = useState(0);
@@ -27,22 +27,24 @@ const useAnimation = (duration) => {
 const Home = () => {
 
     const progress = useAnimation(1000)
-    
+
     return (
-        <div>
-            <h1 className="title " style={{fontSize:'7em'}}>On The Ball</h1>
-              <h6 className="title"style={{float:'right', fontSize:'1.5em',letterSpacing:'.1em', borderColor:'black',borderStyle:'solid', backgroundColor:'black', color:'white'}}> Track client progress</h6>
+        <motion.div
+            animate={{ x: 10 }}
+            transition={{ duration: 2 }}
+            exit={{}}
+        >
+            <h1 className="title " style={{ fontSize: '7em' }}>On The Ball</h1>
+            <h6 className="title" style={{ float: 'right', fontSize: '1.5em', letterSpacing: '.1em', borderColor: 'black', borderStyle: 'solid', backgroundColor: 'black', color: 'white' }}> Track client progress</h6>
             {/* <div className="" style={{display:'inline-block',borderColor:'black', borderStyle:'solid', float:'right'}}>
                 <h2 className="title" style={{fontSize:'3em', borderStyle:'dotted', display:'inline-block'  }}>Welcome</h2>
                 </div> */}
-        <svg viewBox="0 0 100 100">
-            <circle cx="10" cy="10" r="5" />
-            <circle cx={progress * 100} cy="20" r="5" />
-            <circle cx="20" cy={progress * 50} r="10" />
-        </svg>
-        
-       
-        </div>
+            <svg viewBox="0 0 100 100">
+                <circle cx="10" cy="10" r="5" />
+                <circle cx={progress * 100} cy="20" r="5" />
+                <circle cx="20" cy={progress * 50} r="10" />
+            </svg>
+        </motion.div>
     )
 }
 
