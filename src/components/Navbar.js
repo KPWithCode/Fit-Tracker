82e4059ce520780412c5ffd308f5a94c174899b0
Kyle Phillips <khillips375@yahoo.com>
2019-12-29 15:19:11 -0500

    Spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav style={{backgroundColor:'black', color:'white'}} className=" title navbar text-white navbar-dark bg-dark navbar-expand-md">
-        <Link to="/" className="navbar-brand">Get on The ball</Link>
+      <nav style={{backgroundColor:'black', color:'white', marginTop:'5%', borderRadius:'4%'}} className=" title navbar text-white navbar-dark bg-dark navbar-expand-md">
+        <Link to="/" className="navbar-brand">Get On The Ball</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
@@ -16,7 +16,7 @@ export default class Navbar extends Component {
           <Link to="/create" className="nav-link">Create Exercise Log</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/user" className="nav-link">Add User</Link>
+          <Link to="/user" className="nav-link">Add Client</Link>
           </li>
         </ul>
         </div>
